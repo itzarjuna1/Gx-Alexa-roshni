ca4d4d3ed732297b98d2ec1c381fbc4631020b80
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-01 16:44:18 +0530

    Update mongo.py

--- AlexaMusic/core/mongo.py
+++ AlexaMusic/core/mongo.py
@@ -1,30 +1,25 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
 """
 TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
+Copyright (c) 2021 ~ Present Team Alexa
 
 This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
+as you want or you can collab if you have new ideas.
 """
 
-
 from motor.motor_asyncio import AsyncIOMotorClient
-
 from config import MONGO_DB_URI
-
 from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
+
 try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Alexa
+    mongo_client = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = mongo_client["Alexa"]  # or use .get_database() if dynamic
+    db = mongo_client["subscriptions"]  # Your second DB
     LOGGER(__name__).info("Connected to your Mongo Database.")
-except Exception:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+except Exception as e:
+    LOGGER(__name__).error(f"Failed to connect to your Mongo Database: {e}")
     exit()
 
-## Database For Broadcast Subscription By Team Alexa
-MONGODB_CLI = AsyncIOMotorClient(MONGO_DB_URI)
-db = MONGODB_CLI["subscriptions"]
+# Export client if needed elsewhere
+MONGODB_CLI = mongo_client
