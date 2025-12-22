5c5eda6bfb76ea357e7872ca48dcc320a83665d3
ARJUN <pachangedinanath@gmail.com>
2025-12-22 20:41:21 +0530

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -12,6 +12,13 @@ as you want or you can collabe if you have new ideas.
 
 import sys
 
+import asyncio
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    
 from pyrogram import Client
 import config
 from ..logging import LOGGER
