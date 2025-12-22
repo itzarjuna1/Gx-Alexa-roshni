721e0de20078bba2198d801c3e5f4321d8ad2c47
ARJUN <pachangedinanath@gmail.com>
2025-12-22 21:56:18 +0530

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -1,28 +1,20 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
-import sys
+# Copyright (C) 2025 by Alexa_Help
+# https://github.com/TheTeamAlexa
 
 import asyncio
+import sys
 
+# Ensure event loop exists (Python 3.10+ fix)
 try:
     asyncio.get_event_loop()
 except RuntimeError:
     asyncio.set_event_loop(asyncio.new_event_loop())
-    
+
 from pyrogram import Client
+from pyrogram.enums import ChatMemberStatus
+
 import config
 from ..logging import LOGGER
-from pyrogram.enums import ChatMemberStatus
 
 
 class AlexaBot(Client):
@@ -38,25 +30,37 @@ class AlexaBot(Client):
 
     async def start(self):
         await super().start()
+
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
         self.mention = get_me.mention
-        try:
-            await self.send_message(
-                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
-            )
-        except Exception:
-            LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
-            )
-            sys.exit()
-        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
-            sys.exit()
+
+        # 🔕 LOG GROUP CHECK (NON-FATAL)
+        if config.LOG_GROUP_ID:
+            try:
+                await self.send_message(
+                    config.LOG_GROUP_ID,
+                    "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
+                )
+
+                member = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+                if member.status != ChatMemberStatus.ADMINISTRATOR:
+                    LOGGER(__name__).warning(
+                        "Bot is not admin in log group. Logging will be limited."
+                    )
+
+            except Exception as e:
+                LOGGER(__name__).warning(
+                    f"Log group not accessible, skipping logs. Reason: {e}"
+                )
+        else:
+            LOGGER(__name__).warning("LOG_GROUP_ID not set. Skipping log group checks.")
+
+        # Bot name
         if get_me.last_name:
             self.name = f"{get_me.first_name} {get_me.last_name}"
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+
+        LOGGER(__name__).info(f"MusicBot Started Successfully as {self.name}")
