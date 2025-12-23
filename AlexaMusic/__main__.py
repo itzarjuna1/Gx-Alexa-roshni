996eed877fdbbf37f576f1a8aefc1f49619dd0c3
ARJUN <pachangedinanath@gmail.com>
2025-12-23 16:30:57 +0530

    Update __main__.py

--- AlexaMusic/__main__.py
+++ AlexaMusic/__main__.py
@@ -1,18 +1,5 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import asyncio
-
 import importlib
-from typing import Any
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
@@ -27,12 +14,13 @@ from AlexaMusic.utils.database import get_banned_users, get_gbanned
 from AlexaMusic.core.cookies import save_cookies
 
 
-async def init() -> None:
-    # Check for at least one valid Pyrogram string session
+async def init():
     if all(not getattr(config, f"STRING{i}") for i in range(1, 6)):
         LOGGER("AlexaMusic").error("Add Pyrogram string session and then try...")
-        exit()
+        return
+
     await sudo()
+
     try:
         for user_id in await get_gbanned():
             BANNED_USERS.add(user_id)
@@ -40,30 +28,34 @@ async def init() -> None:
             BANNED_USERS.add(user_id)
     except Exception:
         pass
+
     await app.start()
     await save_cookies()
+
     for module in ALL_MODULES:
         importlib.import_module(f"AlexaMusic.plugins.{module}")
+
     LOGGER("AlexaMusic.plugins").info("Necessary Modules Imported Successfully.")
+
     await userbot.start()
     await Alexa.start()
+
     try:
         await Alexa.stream_call("https://telegra.ph/file/b60b80ccb06f7a48f68b5.mp4")
     except NoActiveGroupCall:
-        LOGGER("AlexaMusic").error(
-            "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
-        )
-        exit()
+        LOGGER("AlexaMusic").error("Turn on group voice chat.")
+        return
     except Exception:
         pass
+
     await Alexa.decorators()
     LOGGER("AlexaMusic").info("Alexa Music Bot Started Successfully")
+
     await idle()
+
     await app.stop()
     await userbot.stop()
-    LOGGER("AlexaMusic").info("Stopping Alexa Music Bot...")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
-    LOGGER("AlexaMusic").info("Stopping Music Bot")
+    asyncio.run(init())
